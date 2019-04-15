2d6b228219729d32782eec09f0a413f4ad9d9a63
Aaron Eubank <eubank.aaron@gmail.com>
2019-04-15 14:02:28 -0400

    Added doc with our initial framework concept

---
+++ docs/Framework.R
@@ -0,0 +1,12 @@
+#Framework 
+
+#Use intersect to determine true postive area of each polygon
+
+#USe Differencing for to determine error outside of truth polygon (false Postive)
+
+#area that is within the truth polygon that does not intersect with the mechanical turks polygon
+#compare intersected area with area of truth (false negative)
+
+#somehow eliminate all mechanical Turks polygons that do not have centroid or significantly intersect with truth polygon
+
+#rmap_accuracy
