ea56d8ae2ee182f291102b4269fd0ef7a88b339f
Aaron Eubank <eubank.aaron@gmail.com>
2019-04-17 09:24:42 -0400

    Modified framework files and files from previous Map Africa Package last year

--- docs/Framework.R
+++ docs/Framework.R
@@ -9,4 +9,21 @@
 
 #somehow eliminate all mechanical Turks polygons that do not have centroid or significantly intersect with truth polygon
 
-#rmap_accuracy
+#rmap_accuracy package
+
+
+
+##looping framework
+
+# For each ground truth polygon (i)  {for i in truth}
+# Find the grids it intersects with (j)  {return j that i intersects with}
+# For each grid j, select the corresponding assignments mapped there (k) {for j in fgrids}
+# From each selected assignment k, find:  
+#   if there is a field in k that intersects truth polygon i:
+#   then calculate the accuracy statistics for that intersecting field:
+#   true positive area
+# false positive area
+# false negative area
+# if there is no intersecting field,
+# then record:
+#   false negative area
